84c2e9d4101dc5103407647a8c60e7f2e617cfff
Abraham Amaya <abramovich.amaya@gmail.com>
2021-05-04 23:52:59 -0500

    Hola

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
     <title>Herramienta de apoyo al psicólogo</title>
 </head>
 <body>
-    
+    //holallll
     <div class="title">
         <h1>Bienvenido</h1>
         <h2>Herramienta de apoyo al psicólogo en la evaluación de la ansiedad en jóvenes universitarios.</h2> 
